a50f01d5c650cf5def4391bab854d7d54192cf28
solomase <solomon.asefaw12@gmail.com>
2023-06-19 15:44:12 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A program that prints the size of various computer types
+ * Return: 0 (Success)
  */
 int main(void)
 {
